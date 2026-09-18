035d6e5bf44ac4fb38960886323a38c2b01d13e3
Stellar-Nucleosynthesis <stepanenkonazar356@gmail.com>
2026-09-18 16:16:25 +0300

    feat(#1): add basic project configuration

---
+++ src/main/java/org/spring/divas/userservice/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package org.spring.divas.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+}
