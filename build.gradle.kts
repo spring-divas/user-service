c1e519d81df141d9f6735d5eafdc37e0c6d42566
Stellar-Nucleosynthesis <58738176+Stellar-Nucleosynthesis@users.noreply.github.com>
2026-09-19 12:24:18 +0300

    feat(#2): add user entity and role enum (#7)

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,13 @@ repositories {
 }
 
 dependencies {
+    compileOnly("org.projectlombok:lombok:1.18.48")
+    annotationProcessor("org.projectlombok:lombok:1.18.48")
+
+    implementation("jakarta.persistence:jakarta.persistence-api:4.0.0-M7")
+    implementation("jakarta.validation:jakarta.validation-api:4.0.0-M1")
     implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
---
+++ src/main/java/org/spring/divas/userservice/common/enums/UserRole.java
@@ -0,0 +1,5 @@
+package org.spring.divas.userservice.common.enums;
+
+public enum UserRole {
+    CUSTOMER, MANAGER, ADMIN
+}
