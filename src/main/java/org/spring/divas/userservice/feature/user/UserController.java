2facce4568ded0cc0d205a7b2422911043c7f802
Stellar-Nucleosynthesis <stepanenkonazar356@gmail.com>
2026-09-19 15:21:35 +0300

    feat(#4): add port and path settings to application properties

--- src/main/java/org/spring/divas/userservice/feature/user/UserController.java
+++ src/main/java/org/spring/divas/userservice/feature/user/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/user")
 @RequiredArgsConstructor
 public class UserController {
 
