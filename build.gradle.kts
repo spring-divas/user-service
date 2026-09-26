92fd16ae770900cf8aa0695538bfe65fe15b6e3c
Stellar-Nucleosynthesis <58738176+Stellar-Nucleosynthesis@users.noreply.github.com>
2026-09-26 21:07:50 +0300

    chore(#10): refactor dependency version handling (#33)

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,13 @@ plugins {
     id("io.spring.dependency-management") version "1.1.7"
 }
 
+val lombokVersion = "1.18.48"
+val mapstructVersion = "1.6.3"
+val lombokMapstructBindingVersion = "0.2.0"
+val postgresqlVersion = "42.7.8"
+val h2Version = "2.3.232"
+val checkstyleVersion = "13.7.0"
+
 group = "org.spring.divas"
 version = "0.0.1-SNAPSHOT"
 description = "user-service"
@@ -20,26 +27,29 @@ repositories {
 }
 
 dependencies {
-    compileOnly("org.projectlombok:lombok:1.18.48")
-    annotationProcessor("org.projectlombok:lombok:1.18.48")
-    annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")
-    annotationProcessor("org.projectlombok:lombok-mapstruct-binding:0.2.0")
+    compileOnly("org.projectlombok:lombok:$lombokVersion")
+    annotationProcessor("org.projectlombok:lombok:$lombokVersion")
 
-    implementation("org.mapstruct:mapstruct:1.6.3")
+    annotationProcessor("org.mapstruct:mapstruct-processor:$mapstructVersion")
+    annotationProcessor("org.projectlombok:lombok-mapstruct-binding:$lombokMapstructBindingVersion")
+
+    implementation("org.mapstruct:mapstruct:$mapstructVersion")
+
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
-    runtimeOnly("org.postgresql:postgresql")
+
+    runtimeOnly("org.postgresql:postgresql:$postgresqlVersion")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testRuntimeOnly("com.h2database:h2")
+    testRuntimeOnly("com.h2database:h2:$h2Version")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 checkstyle {
-    toolVersion = "13.7.0"
+    toolVersion = checkstyleVersion
     configFile = file("checkstyle/checks.xml")
     maxWarnings = 0
 }
