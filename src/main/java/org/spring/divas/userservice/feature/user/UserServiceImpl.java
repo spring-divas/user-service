bf9d0c5ef8174a9ff944c21424ca7d858e0302a7
Stellar-Nucleosynthesis <58738176+Stellar-Nucleosynthesis@users.noreply.github.com>
2026-09-26 20:43:00 +0300

    chore(#13): move transaction annotation from the class to methods (#32)

--- src/main/java/org/spring/divas/userservice/feature/user/UserServiceImpl.java
+++ src/main/java/org/spring/divas/userservice/feature/user/UserServiceImpl.java
@@ -1,31 +1,33 @@
 package org.spring.divas.userservice.feature.user;
 
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
 public class UserServiceImpl implements UserService {
 
     private final UserRepository repository;
     private final UserMapper mapper;
 
     @Override
+    @Transactional
     public UserResponseDto create(UserCreateDto dto) {
         User savedUser = repository.save(mapper.toEntity(dto));
         return mapper.toResponseDto(savedUser);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public UserResponseDto findById(Long id) {
         return mapper.toResponseDto(repository.findById(id).orElseThrow());
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<UserResponseDto> findAll() {
         return repository.findAll()
                 .stream()
@@ -34,6 +36,7 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @Transactional
     public UserResponseDto update(Long id, UserUpdateDto dto) {
         User user = repository.findById(id).orElseThrow();
         mapper.updateEntity(dto, user);
@@ -41,6 +44,7 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @Transactional
     public UserResponseDto updatePassword(Long id, ChangePasswordDto dto) {
         User user = repository.findById(id).orElseThrow();
         user.setPassword(dto.getNewPassword());
@@ -48,6 +52,7 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @Transactional
     public void delete(Long id) {
         repository.findById(id).orElseThrow();
         repository.deleteById(id);
